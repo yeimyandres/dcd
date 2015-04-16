96fe31640efc965cf30eaad7f624187a6313b2e6
Andrés Arteaga <yeimyandres@gmail.com>
2015-04-16 10:57:39 -0500

    cambios varios
    varios cambios buscando optimizar

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="./css/estilologin.css" />
-
+	<link rel="shortcut icon" href="./img/favicon.ico" />
 </head>
 
 <body>
